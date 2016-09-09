61e39c35e78bc6da4a2c05731d61c7724c980281
JingkaiZhao <jingkai.zhao@foxmail.com>
2016-09-09 21:06:53 +0800

    remove old assets

--- routes/entry.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('entry', { title: 'Atrical title' });
-});
-
-module.exports = router;
