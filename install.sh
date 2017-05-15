3972e3c4914651429e745d3dbe19134c2e60a2cf
jkzing <jingkai.zhao@foxmail.com>
2017-05-16 01:38:26 +0800

    Add npm install for theme tranquilpeak

--- install.sh
+++ install.sh
@@ -6,3 +6,6 @@ openssl aes-256-cbc -K $encrypted_a44a41e1492a_key -iv $encrypted_a44a41e1492a_i
 rm deploy-key.enc # Don't need it anymore
 chmod 600 deploy-key
 mv deploy-key ~/.ssh/id_rsa
+
+cd themes/tranquilpeak
+npm install
